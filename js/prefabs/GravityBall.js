b7d8e36e75cab741bfd58476fd35af236cedd3d8
Gigi Bachtel <gbachtel@ucsc.edu>
2018-05-07 16:25:03 -0700

    Added tweening to the ball

--- js/prefabs/GravityBall.js
+++ js/prefabs/GravityBall.js
@@ -2,6 +2,7 @@ var GravityBall = function(game, player, frame){
 	// to access these variables, use GravityBall instead of this (GravityBall.deployed)
 	this.deployed = false ;	// whether or not the ball has been deployed
 	this.activated = false; //whether or not the ball is exerting gravity
+	this.tweening = false ; // whether or not the ball is currently tweening
 	//this.direction = false ; // the direction of gravity; true = push, false = pull
 	//this.controls ; // controls for the ball
 
@@ -46,7 +47,7 @@ GravityBall.prototype.update = function() {
 			returnGravityBall(this.game, this.player, this) ;
 		}
 	}
-	else if(!this.deployed && !this.activated) { // if the gravity ball is not deployed
+	else if(!this.deployed && !this.activated && !this.tweening) { // if the gravity ball is not deployed
 		// update the gravity ball's position
 		this.body.x = this.player.body.x - this.player.width ;
 		this.body.y = this.player.body.y ;
@@ -87,13 +88,24 @@ setActivated = function(gravityball)
 	console.log("Gravity Ball Activated");
 }
 
+// set the ball to not tweening so it follows the player again
+setNotTweening = function(gravityball) {
+	gravityball.tweening = false ;
+	console.log("Gravity Ball Finished Tweening") ;
+}
+
+
 deployGravityBall = function(game, player, gravityball){
 	console.log(gravityball.body.x) ;
 	gravityball.deployed = true ; // set deployed equal to true
-	
+
+	// tween the ball over to where the mouse is in 800 ms 
+	// game.add.tween(object).to({properties to tween to}, time (in ms), easing, auto-start) 
+	game.add.tween(gravityball).to({ x: game.input.activePointer.x, y: game.input.activePointer.y }, 800, Phaser.Easing.Elastic.Out, true) ;
+
 	// put the gravity ball where the mouse (or touch) is
-	gravityball.body.x = game.input.activePointer.x ;
-	gravityball.body.y = game.input.activePointer.y ;
+	//gravityball.body.x = game.input.activePointer.x ;
+	//gravityball.body.y = game.input.activePointer.y ;
 	
 	//activate the gravity ball after a short delay
 	setActivationDelay(gravityball);
@@ -101,12 +113,18 @@ deployGravityBall = function(game, player, gravityball){
 
 returnGravityBall = function(game, player, gravityball){
 	console.log(gravityball.body.x) ;
-	gravityball.deployed = false ; // set deployed equal to false
 	gravityball.activated = false; //deactivate the ball
+	gravityball.deployed = false ; // set deployed equal to false
+
+	// tween the ball to the player is in 100 ms 
+	// game.add.tween(object).to({properties to tween to}, time (in ms), easing, auto-start)
+	game.add.tween(gravityball).to({ x: (player.body.x - player.width), y: player.body.y }, 100, Phaser.Easing.Quadratic.Out, true) ;
+	gravityball.tweening = true ; // the ball is now tweening
+	game.time.events.add(Phaser.Timer.SECOND * 0.1, setNotTweening, this, gravityball); // after 100 ms, the ball is no longer tweening
 
 	// move the gravity ball back behind the player
-	gravityball.body.x = player.body.x - player.width ;
-	gravityball.body.y = player.body.y ;
+	//gravityball.body.x = player.body.x - player.width ;
+	//gravityball.body.y = player.body.y ;
 	
 	//for each object being influenced, from the top of the array
 	for(var i = gravityball.influencedArray.length - 1; i >= 0; i--)
@@ -121,5 +139,6 @@ returnGravityBall = function(game, player, gravityball){
 		//remove the object from the array
 		gravityball.influencedArray.pop();
 	}
-	
+
+
 }

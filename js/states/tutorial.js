14cd6406b287ca7617bec64043936fe15250cf67
Gigi Bachtel <gbachtel@ucsc.edu>
2018-05-10 17:39:30 -0700

    implemented tilesheet

--- js/states/tutorial.js
+++ js/states/tutorial.js
@@ -8,9 +8,36 @@ Tutorial.prototype =
 		game.load.path = 'assets/img/sprites/';
 		game.load.spritesheet('idle', 'idle.png', 49, 64);
 		game.load.image('ball', 'gravityball.png');
+
+		// load spritesheet and tilemap for terrain
+		game.load.path = 'assets/img/terrain/';
+		game.load.spritesheet('tutorialtiles', 'tutorialtiles.png', 32, 32) ;
+		game.load.tilemap('map', 'tutorial_map.json', null, Phaser.Tilemap.TILED_JSON);
 	},
 	create: function()
 	{
+		//enable physics system
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		
+
+		// add tileset from json file
+		this.terrain = game.add.tilemap('map') ;
+
+		// add image for the tileset
+		this.terrain.addTilesetImage('tutorial_tiles', 'tutorialtiles') ;
+		
+		// create layers
+		this.bg = this.terrain.createLayer('Background') ; // background layer
+		this.bg.resizeWorld() ; // resize the world so it's the size of the background
+
+		this.bgobj = this.terrain.createLayer('Background Objects') ; // background objects layer
+		this.ground = this.terrain.createLayer('Ground') ; // ground layer
+
+		// set collision for the ground tiles on the ground layer
+		// tilemap.setCollision([tiles], collide (boolean), layer)
+		this.terrain.setCollision([12, 20], true, 'Ground') ;
+
+
 		//*****TAKE OUT LATER*****
 		//display state switching text
 		stateText1 = game.add.text(8, 8, 'State: Tutorial', 
@@ -18,14 +45,12 @@ Tutorial.prototype =
 		stateText2 = game.add.text(8, 32, 'Press S to switch states.', 
 									{font: 'Courier New', fontSize: '24px', fill: "#FFF"});
 		
-		//enable physics system
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-		
 		//set variable for global gravity setting
 		worldGravity = 1000;
 		
 		//create player object using prefab
-		player = new Player(game, 420, 420, 'idle', 0);
+		player = new Player(game, 100, 100, 'idle', 0); 
+		this.player = player ; // create pointer to the player object so it can be accessed from update()
 		
 		//add player animations
 		player.animations.add('idle', [0, 1, 2, 3, 4], 10, true);
@@ -44,6 +69,9 @@ Tutorial.prototype =
 	},
 	update: function()
 	{
+		// collide the player with the ground
+		game.physics.arcade.collide(this.ground, this.player) ;
+
 		//*****TAKE OUT LATER*****
 		//switch states when player presses s
 		if(S.justPressed())
